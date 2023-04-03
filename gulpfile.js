0b769f84880baaa5be176a91386cb5caa91947ea
Vadim Orlov <vv-alp@mail.ru>
2023-04-04 01:04:31 +0300

    added "import/extensions" in lint

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,50 @@
-const {watch, series, parallel} = require("gulp");
-const browserSync = require("browser-sync").create();
+const { watch, series, parallel } = require('gulp');
+const browserSync = require('browser-sync').create();
 
-//Конфигурация
-const path = require("./config/path.js");
-const app = require("./config/app.js");
+// Конфигурация
+const path = require('./config/path.js');
+const app = require('./config/app.js');
 
-//задачи
-const clear = require("./task/clear.js");
-const html = require("./task/html.js");
-const scss = require("./task/scss.js");
-const js = require("./task/js.js");
-const img = require("./task/img.js");
-const photos = require("./task/photos.js");
-const fonts = require("./task/fonts.js");
+// задачи
+const clear = require('./task/clear.js');
+const html = require('./task/html.js');
+const scss = require('./task/scss.js');
+const js = require('./task/js.js');
+const img = require('./task/img.js');
+const photos = require('./task/photos.js');
+const fonts = require('./task/fonts.js')
 
-
-//Наблюдение
+// Наблюдение
 const watcher = () => {
-  watch(path.html.watch, html).on("all", browserSync.reload);
-  watch(path.scss.watch, scss).on("all", browserSync.reload);
-  watch(path.js.watch, js).on("all", browserSync.reload);
-  watch(path.img.watch, img).on("all", browserSync.reload);
-  watch(path.photos.watch, photos).on("all", browserSync.reload);
-  watch(path.fonts.watch, fonts).on("all", browserSync.reload);
+  watch(path.html.watch, html).on('all', browserSync.reload);
+  watch(path.scss.watch, scss).on('all', browserSync.reload);
+  watch(path.js.watch, js).on('all', browserSync.reload);
+  watch(path.img.watch, img).on('all', browserSync.reload);
+  watch(path.photos.watch, photos).on('all', browserSync.reload);
+  watch(path.fonts.watch, fonts).on('all', browserSync.reload);
 };
 
-
-//Сервер
-const server = ()=> {
+// Сервер
+const server = () => {
   browserSync.init({
     server: {
-      baseDir: "public/"
-    }
+      baseDir: 'public/',
+    },
   });
-
 }
 
 const build = series(
   clear,
-  parallel(scss, html, js, img, fonts, photos)
+  parallel(scss, html, js, img, fonts, photos),
 );
 
-//Сборка
+// Сборка
 const dev = series(
   build,
-  parallel(watcher,server)
+  parallel(watcher, server),
 );
 
-//Задачи
+// Задачи
 exports.html = html;
 exports.scss = scss;
 exports.js = js;
@@ -56,7 +53,7 @@ exports.photos = photos;
 exports.fonts = fonts;
 exports.clear = clear;
 
-//Сборка
+// Сборка
 exports.default = app.isProd
   ? build
   : dev;
