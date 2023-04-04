2d672cf61dc2b34cf862b54898d816711bcdb9fa
Vadim Orlov <vv-alp@mail.ru>
2023-04-04 16:24:01 +0300

    added escdown for big picture and created moduls

---
+++ source/js/new-big-picture.js
@@ -0,0 +1,47 @@
+// ------------ bigPictures open/close
+
+(function () {
+  var bigPictures = document.querySelector('.big-picture');
+
+  // рендер коментария
+  var renderComment = function (bigPicture, index, bigPictureTemplate) {
+    var pictureElement = bigPictureTemplate.cloneNode(true);
+
+    var randomAvatar = Math.floor(Math.random() * 6) + 1;
+    pictureElement.querySelector('.social__picture').src = `img/avatar-${randomAvatar}.svg`;
+    pictureElement.querySelector('.social__text').textContent = bigPicture.comments[index];
+
+    return pictureElement;
+  };
+
+  // рендер коментарии к big-picture
+  var renderBigPictureComments = function (bigPicture) {
+    var bigPictureCommentTemplate = document.querySelector('#social__comment').content.querySelector('.social__comment');
+    var fragment = document.createDocumentFragment();
+
+    for (var i = 0; i < bigPicture.comments.length; i++) {
+      fragment.appendChild(renderComment(bigPicture, i, bigPictureCommentTemplate));
+      document.querySelector('.social__comments').appendChild(fragment);
+    }
+  };
+
+  // Удаления старых коментариев
+  var removeLastBigPictureComments = function () {
+    var listLastComents = document.querySelector('.social__comments').querySelectorAll('.social__comment');
+    for (var i = 0; i < listLastComents.length; i++) {
+      document.querySelector('.social__comments').removeChild(listLastComents[i])
+    }
+  }
+
+  window.newBigPicture = function (itemPictures) {
+    if (itemPictures !== undefined) {
+      bigPictures.querySelector('.big-picture__img').querySelector('img').src = itemPictures.url;
+      bigPictures.querySelector('.likes-count').textContent = itemPictures.likes;
+      bigPictures.querySelector('.comments-count').textContent = itemPictures.comments.length;
+      bigPictures.querySelector('.likes-count').textContent = itemPictures.likes;
+      bigPictures.querySelector('.social__caption').textContent = itemPictures.desckription;
+      removeLastBigPictureComments();
+      renderBigPictureComments(itemPictures);
+    }
+  };
+}());
